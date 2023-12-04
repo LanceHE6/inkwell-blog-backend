535bb7081c886fe6dc57be296e85396209191653
LanceHE6 <2765543491@qq.com>
2023-12-04 11:37:01 +0800

    initial commit

---
+++ src/main/java/com/inkwell/inkwellblog/InkwellBlogApplication.java
@@ -0,0 +1,13 @@
+package com.inkwell.inkwellblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkwellBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InkwellBlogApplication.class, args);
+    }
+
+}
