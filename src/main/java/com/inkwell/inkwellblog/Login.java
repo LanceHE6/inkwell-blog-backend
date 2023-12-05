440cf78412cae5a7c162478f9ea7d1f5dc15caa2
LanceHE6 <2765543491@qq.com>
2023-12-05 08:46:32 +0800

    docs.新增注释

--- src/main/java/com/inkwell/inkwellblog/Login.java
+++ src/main/java/com/inkwell/inkwellblog/Login.java
@@ -6,11 +6,23 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin
 @RequestMapping("/")
 public class Login {
+    /**
+     * login请求示例
+     * @param uid uid参数
+     * @param psw password参数
+     * @return 返回请求的uid和密码
+     */
     @PostMapping("login")
     public UserData logIn(@RequestParam String uid, @RequestParam String psw) {
+        // UserData为数据类，用于封装数据
         UserData userData = new UserData();
+        // 将传入的参数设置给数据类
         userData.setUserId(uid);
         userData.setPassword(psw);
+        // 返回数据 like:
+        /*
+        *{userId: uid, password: psw}
+        * */
         return userData;
     }
 }
